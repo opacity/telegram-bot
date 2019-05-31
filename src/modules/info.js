66ee6791d56a03b77f9ec26c3f3e69bd5b1b9535
Marcel Lindig <ml@nullpilot.de>
2019-05-31 17:46:47 +0200

    add /start message

--- src/modules/info.js
+++ src/modules/info.js
@@ -1,9 +1,17 @@
 import BasicModule from "./basicModule.js";
+import { contextReply, render } from "../util.js";
 
 export default class InfoModule extends BasicModule {
   commands() {
-    this.bot.start(ctx => {
-      ctx.reply("Hello!");
-    })
+    this.bot.start(this.sendInfo);
+  }
+
+  async sendInfo(ctx) {
+    const msg = await render("info");
+
+    await contextReply(ctx, msg, {
+      parse_mode: "HTML",
+      disable_web_page_preview: true
+    });
   }
 }
