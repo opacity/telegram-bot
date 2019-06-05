7643f15628adc85170b30e41c1f86e4ea27fce94
Marcel Lindig <ml@nullpilot.de>
2019-06-05 18:47:12 +0200

    add stats module

--- src/bot.js
+++ src/bot.js
@@ -2,19 +2,26 @@ import Telegraf from "telegraf";
 import RedisModule from "./modules/redis.js";
 import ErrorModule from "./modules/error.js";
 import InfoModule from "./modules/info.js";
+import StatsModule from "./modules/stats.js";
 import DownloadModule from "./modules/download.js";
 import UploadModule from "./modules/upload.js";
 import { isCommand, argumentMiddleware } from "./util.js";
 
 export default async function startBot() {
+  console.log("Initializing Telegram bot..");
+
   const token = process.env.TG_API_TOKEN;
   const bot = new Telegraf(token, { retryAfter: 2 });
   const botInfo = await bot.telegram.getMe();
-  const handle = process.env.OPQ_HANDLE;
   const endpoint = process.env.OPQ_ENDPOINT;
   const redisOpts = {
     keyPrefix: process.env.REDIS_PREFIX || "",
     db: process.env.REDIS_DB || 0
+  }
+  const uploadOpts = {
+    handle: process.env.OPQ_HANDLE,
+    cooldown: process.env.TG_UPLOAD_COOLDOWN,
+    endpoint
   }
 
   bot.options.username = botInfo.username;
@@ -23,14 +30,17 @@ export default async function startBot() {
   // Provide ctx.arg and ctx.argv for all commands
   bot.use(Telegraf.optional(isCommand, argumentMiddleware));
 
+  console.log("Loading modules..");
   await RedisModule.start(bot, redisOpts);
   await ErrorModule.start(bot);
   await InfoModule.start(bot);
+  await StatsModule.start(bot);
   await DownloadModule.start(bot, { endpoint });
-  await UploadModule.start(bot, { handle, endpoint });
+  await UploadModule.start(bot, uploadOpts);
 
-  console.log(`Starting Telegram bot @${bot.options.username}.`);
   bot.startPolling();
+
+  console.log(`Started Telegram bot @${bot.options.username}!`);
 
   return bot;
 }
---
+++ src/modules/stats.js
@@ -0,0 +1,111 @@
+import bytes from "bytes"
+import moment from "moment";
+import format from "format-number"
+import BasicModule from "./basicModule.js";
+import { contextReply, render } from "../util.js";
+
+const TIME_FORMAT = "Y.MM.DD.HH";
+
+export default class StatsModule extends BasicModule {
+  init(bot) {
+    this.redis = bot.context.redis;
+    this.format = format();
+
+    bot.context.logStats = this.logStats.bind(this);
+  }
+
+  commands(bot) {
+    bot.command("stats", this.showStats.bind(this));
+  }
+
+  async showStats(ctx) {
+    const [ uploadStats, downloadStats ] = await this.getStats();
+    const totalUploads = uploadStats.shift().pop();
+    const totalDownloads = downloadStats.shift().pop();
+    const dailyUploads = uploadStats.reduce(this.sumStats, {count: 0, bytes: 0});
+    const dailyDownloads = downloadStats.reduce(this.sumStats, {count: 0, bytes: 0});
+    const format = this.format;
+
+    const msg = await render("stats", {
+      upload: {
+        total: {
+          count: format(parseInt(totalUploads.count || 0)),
+          bytes: bytes(parseInt(totalUploads.bytes || 0))
+        },
+        daily: {
+          count: format(dailyUploads.count || 0),
+          bytes: bytes(dailyUploads.bytes || 0)
+        }
+      },
+      download: {
+        total: {
+          count: format(parseInt(totalDownloads.count || 0)),
+          bytes: bytes(parseInt(totalDownloads.bytes || 0))
+        },
+        daily: {
+          count: format(dailyDownloads.count || 0),
+          bytes: bytes(dailyDownloads.bytes || 0)
+        }
+      }
+    });
+
+    await contextReply(ctx, msg, {
+      parse_mode: "HTML"
+    });
+  }
+
+  async getStats () {
+    let keys = this.getKeys(moment(), 24);
+    let uploadPipe = this.redis.pipeline();
+    let downloadPipe = this.redis.pipeline();
+
+    uploadPipe.hgetall("upload.total");
+    downloadPipe.hgetall("download.total");
+
+    for (let i = 0; i < keys.length; i++) {
+      uploadPipe.hgetall("upload.hourly." + keys[i])
+      downloadPipe.hgetall("download.hourly." + keys[i])
+    }
+
+    return await Promise.all([uploadPipe.exec(), downloadPipe.exec()])
+  }
+
+  getKeys (startTime, count) {
+    const keys = [];
+
+    while (count-- > 0) {
+      const datetime = moment(startTime).subtract(count, "hours");
+      keys.push(datetime.format(TIME_FORMAT));
+    }
+
+    return keys;
+  }
+
+  sumStats (acc, data) {
+    const stats = data.pop();
+
+    acc.count += parseInt(stats.count, 10) || 0;
+    acc.bytes += parseInt(stats.bytes, 10) || 0;
+
+    return acc;
+  }
+
+  async logStats(key, stats) {
+    try {
+      const pipeline = this.redis.pipeline();
+      const totalKey = key + ".total";
+      const datetimeKey = key + ".hourly." + moment().format(TIME_FORMAT);
+
+      Object.keys(stats).forEach(field => {
+        const val = stats[field];
+
+        pipeline.hincrby(totalKey, field, val);
+        pipeline.hincrby(datetimeKey, field, val)
+      });
+
+      return await pipeline.exec();
+    } catch(err) {
+      console.warn(err);
+    }
+  }
+}
